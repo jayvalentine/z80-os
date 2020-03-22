f1ffee1d9ed0224f0edb86736946330b0917165c
jayvalentine <jayv136@gmail.com>
2020-03-22 18:58:34 +0000

    Added repeating NMI

--- os.asm
+++ os.asm
@@ -8,17 +8,43 @@
 start:
     jp      main
 
-    org $0100
-main:
-    ; Register initialization
-    ld      SP, $ffff
-
+    org $0066
+handler_nmi:
     ld      HL, prompt
     ld      B, 6
 
     call    print
 
-    halt
+    ; Restart the timer.
+    ld      A, $01
+    out     (14), A
+    
+    retn
+
+    org $0100
+main:
+    ; Register initialization
+    ld      SP, $ffff
+
+    ; Timer initialization.
+    ; 1s interval - 8,000,000 cycles @ 8MHz.
+    ; Timer value needs to be 0x7a1200.
+    ld      A, $00
+    out     (10), A
+    ld      A, $12
+    out     (11), A
+    ld      A, $7a
+    out     (12), A
+    ld      A, $00
+    out     (13), A
+
+    ; Start the timer.
+    ld      A, $01
+    out     (14), A
+
+    ; Infinite loop.
+loop:
+    jp      loop
 
 ; Prints a text string, followed by a newline.
 ;
