7d294463e02c943d460516e6d6572ae9fd38b5f4
jayvalentine <jayv136@gmail.com>
2020-03-27 09:55:30 +0000

    Added sleep function

--- os.asm
+++ os.asm
@@ -10,6 +10,11 @@ start:
 
     org $0066
 handler_nmi:
+    push    AF
+    push    BC
+    push    HL
+    push    DE
+
     ld      HL, prompt
     ld      B, 6
 
@@ -18,6 +23,11 @@ handler_nmi:
     ; Restart the timer.
     ld      A, $01
     out     (14), A
+
+    pop     DE
+    pop     HL
+    pop     BC
+    pop     AF
     
     retn
 
@@ -27,13 +37,13 @@ main:
     ld      SP, $ffff
 
     ; Timer initialization.
-    ; 1s interval - 8,000,000 cycles @ 8MHz.
-    ; Timer value needs to be 0x7a1200.
-    ld      A, $00
+    ; 1s interval - 400,000 cycles @ 400KHz.
+    ; Timer value needs to be 0x00061a80.
+    ld      A, $80
     out     (10), A
-    ld      A, $12
+    ld      A, $1a
     out     (11), A
-    ld      A, $7a
+    ld      A, $06
     out     (12), A
     ld      A, $00
     out     (13), A
@@ -42,9 +52,64 @@ main:
     ld      A, $01
     out     (14), A
 
-    ; Infinite loop.
-loop:
-    jp      loop
+task_A:
+    ld      HL, task_A_string
+    ld      B, 3
+    call    print
+
+    ; Sleep for 2 seconds.
+    ld      HL, $07d0
+    call    sleep
+
+    jp      task_A
+
+task_A_string:
+    text    "ABC"
+
+task_B:
+    ld      HL, task_B_string
+    ld      B, 3
+    call    print
+
+    
+    jp      task_B
+
+task_B_string:
+    text    "xyz"
+
+; Waits for a given amount of time (in milliseconds)
+;
+; Parameters:
+;   HL - the time to wait in milliseconds.
+sleep:
+    ; Push BC as we're going to trash B.
+    push    BC
+    ; 1 millisecond is 400 cycles, at 400KHz.
+
+    ; We want to construct an inner loop that runs for 1 millisecond.
+    ; Then we just execute that inner loop for as many times as HL.
+sleep_outer:
+    ; A DJNZ takes ~13 cycles to execute.
+    ; So, our inner loop wants to execute roughly 400/13 times.
+    ;
+    ; Actually, we want to execute one less, to account for the time
+    ; the outer jump will take.
+
+    ld      B, 29
+
+sleep_inner:
+    djnz    sleep_inner
+
+    ; Decrement HL. Is it 0?
+    dec     HL
+    ld      A, H
+    or      A, L
+    jp      nz, sleep_outer
+
+sleep_done:
+    ; Pop BC and return.
+    pop     BC
+    ret
 
 ; Prints a text string, followed by a newline.
 ;
