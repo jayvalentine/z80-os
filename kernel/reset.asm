909c60cc1adbe677326f58b4b75a7bd12a982697
jayvalentine <jayv136@gmail.com>
2020-11-04 11:55:10 +0000

    Added proper reset vectors

--- kernel/reset.asm
+++ kernel/reset.asm
@@ -1,11 +1,19 @@
     ; Reset vectors for Z80-OS kernel.
 
-    EXTERN  _main
-
-_reset:
-    ld      A, 0b01010101
-    out     ($80), A
-    call    _main
-    halt
+    EXTERN  _start
+    EXTERN  _syscall_handler
+    EXTERN  _interrupt_handler
     
-    defs    $1234
+    ; Entry point.
+_reset:
+    ; Disable interrupts on startup.
+    di
+    jp      _start
+
+    defs    0x0030 - ASMPC
+_syscall_entry:
+    jp      _syscall_handler
+
+    defs    0x0038 - ASMPC
+_interrupt_entry:
+    jp      _interrupt_handler
