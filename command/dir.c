d1a573c65f130c834c7f98b7f061ccc3a852a0f2
jayvalentine <jayv136@gmail.com>
2020-11-08 20:02:32 +0000

    DIR command now uses the new syscalls

--- command/dir.c
+++ command/dir.c
@@ -9,60 +9,18 @@ extern char temp[512];
 int command_dir(char ** argv, size_t argc)
 {
     char filename[13];
+    FINFO finfo;
 
-    const DiskInfo_T * dinfo = syscall_dinfo();
+    uint16_t file_entries = syscall_fentries();
 
-    uint32_t sector = dinfo->root_region;
-
-    uint8_t done = 0;
-    while (!done)
+    for (uint16_t f = 0; f < file_entries; f++)
     {
-        /* Read this sector of the root directory. */
-        syscall_dread(temp, sector);
-
-        for (size_t f = 0; f < 512; f += 32)
+        int error = syscall_fentry(filename, f);
+        if (error == 0)
         {
-            /* If the first byte of the file is 0, stop the search. */
-            if (temp[f] == 0) return 0;
-
-            /* If the first byte is e5, this entry is free, so we should skip it. */
-            if (temp[f] == 0xe5) continue;
-
-            /* Check that this is a file. Skip if not. */
-            uint8_t attr = temp[f+11];
-            if (attr & 0b00011000) continue;
-
-            /* Construct the filename. */
-            memcpy(&filename[0], &temp[f], 8);
-            
-            size_t sep_pos;
-            for (sep_pos = 0; sep_pos < 8; sep_pos++)
-            {
-                if (filename[sep_pos] == ' ') break;
-            }
-
-            size_t ext_len;
-            for (ext_len = 0; ext_len < 3; ext_len++)
-            {
-                if (temp[f+8+ext_len] == ' ') break;
-            }
-
-            /* If no extension, don't insert the '.' */
-            if (ext_len == 0)
-            {
-                filename[sep_pos] = '\0';
-            }
-            else
-            {
-                filename[sep_pos] = '.';
-                memcpy(&filename[sep_pos+1], &temp[f+8], ext_len);
-                filename[sep_pos+1+ext_len] = '\0';
-            }
-
+            puts("    ");
             puts(filename);
             for (uint8_t i = strlen(filename); i < 20; i++) putchar(' ');
-
-            FINFO finfo;
 
             syscall_finfo(filename, &finfo);
             
@@ -79,7 +37,11 @@ int command_dir(char ** argv, size_t argc)
 
             printf("  %04u-%02u-%02u\n\r", finfo.created_year, (uint16_t)finfo.created_month, (uint16_t)finfo.created_day);
         }
-
-        sector++;
+        else
+        {
+            printf("    Error in file entry %u: %u\n\r", f, error);
+        }
     }
+
+    return 0;
 }
