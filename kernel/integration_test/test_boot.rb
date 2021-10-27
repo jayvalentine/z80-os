97e4cc52be859b1c4a5b32fc40a08180a6e1dd47
Jay Valentine <jayv136@gmail.com>
2021-10-27 21:50:10 +0100

    Renamed base class to make it clear it isn't a test class

--- kernel/integration_test/test_boot.rb
+++ kernel/integration_test/test_boot.rb
@@ -1,20 +1,22 @@
 require 'minitest/autorun'
-require_relative 'test_base'
+require_relative 'base'
 
-def test_boot
-    File.open("test_boot.bin", "wb") do |f|
-        f.write [0, 0, 0, 0].pack("CCCC")
+class BootTest < IntegrationTest
+    def test_boot
+        File.open("test_boot.bin", "wb") do |f|
+            f.write [0, 0, 0, 0].pack("CCCC")
+        end
+
+        start_instance("test_boot.bin")
+
+        # We expect to start executing at 0x6000,
+        # where the command-processor would reside normally.
+        @instance.break 0x6000, :program
+        
+        # Run, and expect to hit the breakpoint.
+        @instance.continue
+
+        assert @instance.break?, "Did not hit breakpoint."
+        assert_equal 0x6000, @instance.registers["PC"], "Breakpoint at wrong address."
     end
-
-    start_instance("test_boot.bin")
-
-    # We expect to start executing at 0x6000,
-    # where the command-processor would reside normally.
-    @instance.break 0x6000, :program
-    
-    # Run, and expect to hit the breakpoint.
-    @instance.continue
-
-    assert @instance.break?, "Did not hit breakpoint."
-    assert_equal 0x6000, @instance.registers["PC"], "Breakpoint at wrong address."
 end
