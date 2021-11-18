dd3e464490178edcd0bbc1cc44a4b1ebc9549622
Jay Valentine <jayv136@gmail.com>
2021-11-18 23:11:25 +0000

    Added test for syscall status LED

--- kernel/integration_test/test_status.rb
+++ kernel/integration_test/test_status.rb
@@ -83,4 +83,83 @@ class StatusTest < IntegrationTest
         status = @instance.zemu_io_status_value
         assert_equal 0b11111111, status, "Status LED still set after exiting ISR!"
     end
+
+    # Tests that the correct status bit gets set
+    # when entering a syscall, and unset when leaving.
+    def test_status_syscall
+        compile_test_code(["kernel/integration_test/test_status_syscall.c"], "test_status_syscall.bin")
+
+        # Get symbols.
+        symbols = []
+        prog_symbols = Zemu::Debug.load_map("test_status_syscall.map")
+        kernel_symbols = Zemu::Debug.load_map("kernel_debug.map")
+        prog_symbols.each do |addr, syms|
+            syms.each do |sym|
+                symbols << sym
+            end
+        end
+        kernel_symbols.each do |addr, syms|
+            syms.each do |sym|
+                symbols << sym
+            end
+        end
+
+        int_breakpoint = symbols.select { |s| s.label == "_do_swrite" }[0]
+        assert !int_breakpoint.nil?, "Could not find symbol for swrite handler!"
+
+        prog_breakpoint = symbols.select { |s| s.label == "_test_func" }[0]
+        assert !prog_breakpoint.nil?, "Could not find symbol for test function!"
+
+        start_instance("test_status_syscall.bin")
+
+        # We expect to start executing at 0x6000,
+        # where the command-processor would reside normally.
+        @instance.break 0x6000, :program
+        
+        # Run, and expect to hit the breakpoint.
+        @instance.continue
+        assert @instance.break?, "Did not hit breakpoint (at address %04x)" % @instance.registers["PC"]
+        assert_equal 0x6000, @instance.registers["PC"], "Breakpoint at wrong address."
+
+        # Syscall status bit should be unset.
+        status = @instance.zemu_io_status_value
+        assert_equal 0b11111111, status, "Status LED unexpectedly set!"
+
+        # Set a breakpoint for the test function.
+        @instance.break prog_breakpoint.address, :program
+        
+        # Continue until we hit the breakpoint.
+        @instance.continue
+        assert @instance.break?, "Did not hit breakpoint (at address %04x)" % @instance.registers["PC"]
+        assert_equal prog_breakpoint.address, @instance.registers["PC"], "Breakpoint at wrong address."
+
+        # Syscall status bit should still be unset.
+        status = @instance.zemu_io_status_value
+        assert_equal 0b11111111, status, "Status LED unexpectedly set!"
+
+        # Remove the program breakpoint, set a breakpoint in the syscall routine.
+        @instance.remove_break prog_breakpoint.address, :program
+        @instance.break int_breakpoint.address, :program
+
+        # Continue, we should hit the syscall breakpoint.
+        @instance.continue
+        assert @instance.break?, "Did not hit breakpoint (at address %04x)" % @instance.registers["PC"]
+        assert_equal int_breakpoint.address, @instance.registers["PC"], "Breakpoint at wrong address."
+
+        # Syscall status bit should now be set.
+        status = @instance.zemu_io_status_value
+        assert_equal 0b11111011, status, "Status LED not set!"
+
+        # Set breakpoint on program.
+        @instance.break prog_breakpoint.address, :program
+        
+        # Continue until we hit the breakpoint.
+        @instance.continue
+        assert @instance.break?, "Did not hit breakpoint (at address %04x)" % @instance.registers["PC"]
+        assert_equal prog_breakpoint.address, @instance.registers["PC"], "Breakpoint at wrong address."
+
+        # Syscall status bit should now be unset.
+        status = @instance.zemu_io_status_value
+        assert_equal 0b11111111, status, "Status LED still set after exiting ISR!"
+    end
 end
---
+++ kernel/integration_test/test_status_syscall.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+volatile int val;
+
+void test_func()
+{
+    val = 42;
+}
+
+int main()
+{
+    while (1)
+    {
+        test_func();
+        putchar('a');
+    }
+}
