4b55f869827faaf6da1b93e6bb33ae2a07cceb59
Jay Valentine <jayv136@gmail.com>
2021-11-20 17:07:21 +0000

    BASIC: More testing of statement interpreter

--- builtin/basic/unit_test/statement.c
+++ builtin/basic/unit_test/statement.c
@@ -68,3 +68,49 @@ int test_interpret_assignment()
 
     ASSERT_EQUAL_INT(3, val);
 }
+
+int test_interpret_assignment_from_self()
+{
+    program_new();
+
+    const char * input = "AM = AM - 4";
+    tok_t dst[80];
+
+    error_t e = statement_tokenize(dst, input);
+    ASSERT_EQUAL_UINT(ERROR_NOERROR, e);
+
+    /* Set up the context. */
+    program_set_numeric("AM", 9);
+
+    error_t e2 = statement_interpret(dst);
+    ASSERT_EQUAL_UINT(ERROR_NOERROR, e2);
+
+    numeric_t val;
+    error_t e3 = program_get_numeric("AM", &val);
+    ASSERT_EQUAL_UINT(ERROR_NOERROR, e3);
+
+    ASSERT_EQUAL_INT(5, val);
+}
+
+int test_interpret_assignment_from_self_equal()
+{
+    program_new();
+
+    const char * input = "AM = AM - AM + AM";
+    tok_t dst[80];
+
+    error_t e = statement_tokenize(dst, input);
+    ASSERT_EQUAL_UINT(ERROR_NOERROR, e);
+
+    /* Set up the context. */
+    program_set_numeric("AM", 9);
+
+    error_t e2 = statement_interpret(dst);
+    ASSERT_EQUAL_UINT(ERROR_NOERROR, e2);
+
+    numeric_t val;
+    error_t e3 = program_get_numeric("AM", &val);
+    ASSERT_EQUAL_UINT(ERROR_NOERROR, e3);
+
+    ASSERT_EQUAL_INT(9, val);
+}
