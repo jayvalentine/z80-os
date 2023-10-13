67af699097370febcb6e74168d204c3d4b15585f
Jay Valentine <jayv136@gmail.com>
2023-10-13 21:44:42 +0100

    Updated main() for text editor

--- builtin/edit/main.c
+++ builtin/edit/main.c
@@ -21,10 +21,19 @@ FILE thefile;
 #define ARG_FIRST 1
 #endif
 
+#define INFO_COL 1
+#define INFO_ROW 41
+
+#define SETPOS(_row, _col) printf("\033[%d;%dH", (_row), (_col))
+#define SETPOS_CONST_(_row, _col) printf("\033[" #_row ";"  #_col "H")
+#define SETPOS_CONST(_row, _col) SETPOS_CONST_(_row, _col)
+
+#define SETPOS_INFO() SETPOS_CONST(INFO_ROW, INFO_COL)
+
 struct
 {
-    uint8_t x;
-    uint8_t y;
+    uint8_t row;
+    uint8_t col;
 } cursor_pos;
 
 void clear_screen(void)
@@ -32,14 +41,41 @@ void clear_screen(void)
     printf("\033[2J\033[1;1H");
 }
 
+void update_info_row(uint8_t newpos)
+{
+    SETPOS_INFO();
+    printf("\033[5C%d", (int)newpos);
+    SETPOS(newpos, cursor_pos.col);
+
+    cursor_pos.row = newpos;
+}
+
+void update_info_col(uint8_t newpos)
+{
+    SETPOS_INFO();
+    printf("\033[14C%d", (int)newpos);
+    SETPOS(cursor_pos.row, newpos);
+
+    cursor_pos.col = newpos;
+}
+
 void handle_key(char c)
 {
     putchar(c);
 
     /* Keep track of cursor position. */
-    if (c == '\r') cursor_pos.x = 1;
-    else if (c == '\n') cursor_pos.y++;
-    else cursor_pos.x++;
+    if (c == '\r')
+    {
+        update_info_col(1);
+    }
+    else if (c == '\n')
+    {
+        update_info_row(cursor_pos.row + 1);
+    }
+    else
+    {
+        update_info_col(cursor_pos.col + 1);
+    }
 }
 
 int main(size_t argc, char ** argv)
@@ -47,9 +83,9 @@ int main(size_t argc, char ** argv)
     argc; argv;
 
     /* Initialize cursor position and clear screen. */
-    cursor_pos.x = 1;
-    cursor_pos.y = 1;
     clear_screen();
+    update_info_row(1);
+    update_info_col(1);
 
     /* Handle keypresses in a loop. */
     while (1)
